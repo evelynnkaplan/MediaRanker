11fd725cc072d51edbbbc2b49712c612ce799e88
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-04-22 16:43:49 -0700

    added works resources and began Works#index action and view

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,13 @@
 class WorksController < ApplicationController
+  
+  def index
+    @works = Work.all
+
+    @movies = Work.where(category: "movie")
+
+    @albums = Work.where(category: "album")
+
+    @books = Work.where(category: "book")
+  end
+
 end
