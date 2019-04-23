41b3d709e08b02400ea4cc7b57d2f130d3f0fe9e
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-04-23 15:41:11 -0700

    Show view for works, error handling includes flash on index view

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -8,4 +8,13 @@ class WorksController < ApplicationController
     @books = Work.where(category: "book")
   end
 
+  def show
+    @work = Work.find_by(id: params[:id])
+
+    if !@work
+      flash[:error] = "No work with that ID found."
+      redirect_to works_path
+    end
+  end
+
 end
