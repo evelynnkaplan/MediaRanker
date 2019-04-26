a02adf9e006dc99b6066708aec9fd3184f252dc5
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-04-26 14:37:20 -0700

    added work fixtures and test for Work.categories

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -6,4 +6,14 @@ describe Work do
   it "must be valid" do
     value(work).must_be :valid?
   end
+
+  describe "categories" do
+    it "must return a list of categories" do
+      categories = Work.categories
+
+      expect(categories).must_include("album")
+      expect(categories).must_include("book")
+      expect(categories).must_include("movie")
+    end
+  end
 end
