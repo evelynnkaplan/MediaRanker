8f9785be254af59fe71828217dcbe961cafa904f
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-04-28 12:46:35 -0700

    wrote index, show, login_form, user_params and find_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,28 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.all
   end
 
+  def show
+    if !user
+      flash[:error] = "No user with that ID found."
+      redirect_to users_path
+    end
+  end
+
   def login_form
     @user = User.new
+  end
+
+  private
+
+  def user_params
+    return params.require(:user).permit(:username)
+  end
+
+  def find_user
+    @user = User.find_by(id: params[:id])
   end
 end
