3a4d4f5ff11ce2e160eb6d81a21642d2a2ee8aee
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-04-28 14:22:50 -0700

    added references to votes for both users and works

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_26_225714) do
+ActiveRecord::Schema.define(version: 2019_04_28_212106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "vote_id"
+    t.index ["vote_id"], name: "index_users_on_vote_id"
+  end
+
+  create_table "votes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -29,6 +36,10 @@ ActiveRecord::Schema.define(version: 2019_04_26_225714) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "vote_id"
+    t.index ["vote_id"], name: "index_works_on_vote_id"
   end
 
+  add_foreign_key "users", "votes"
+  add_foreign_key "works", "votes"
 end
