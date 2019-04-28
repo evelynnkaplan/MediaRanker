c5dd57c222277b3bb6fc0c68da1622382439b591
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-04-28 13:13:05 -0700

    wrote #login action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def login
+    username = params[:user][:username]
+    user = User.find_by(username: username)
+    if !user
+      user = User.create!(username: username)
+      session[:user_id] = user.id
+      flash[:success] = "Successfully created new user #{username} with ID #{user.id}"
+    else
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as existing user #{username}"
+    end
+  end
+
   private
 
   def user_params
