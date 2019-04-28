6df73eba89e8e15b1fdf8246254e0e5f4864fd92
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-04-28 13:32:11 -0700

    added new users routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   resources :works
 
-  resources :users, except: :new
+  resources :users, except: [:new, :create]
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
 end
