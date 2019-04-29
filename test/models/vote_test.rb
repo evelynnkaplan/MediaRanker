f8af403fcd013a318b05e178f65bb4861c33898d
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-04-29 16:16:20 -0700

    wrote vote model tests

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,44 @@
 require "test_helper"
 
 describe Vote do
-  let(:vote) { Vote.new }
+  let (:vote) { Vote.new(user_id: users(:one).id, work_id: works(:gump).id) }
 
-  it "must be valid" do
-    value(vote).must_be :valid?
+  describe "validations" do
+    it "is valid when it's associated with a work and a user" do
+      value(vote).must_be :valid?
+    end
+
+    it "isn't valid when a work_id is missing" do
+      bad_vote = Vote.new(user_id: users(:one).id)
+
+      value(bad_vote).wont_be :valid?
+    end
+
+    it "isn't valid when a user_id is missing" do
+      bad_vote = Vote.new(work_id: works(:gump).id)
+
+      value(bad_vote).wont_be :valid?
+    end
+  end
+
+  describe "voter_name" do
+    it "returns the username of the user associated with the vote" do
+      expect(vote.voter_name).must_equal users(:one).username
+    end
+
+    it "raises an error if the user associated with the vote isn't found, because the vote won't be valid" do
+      bad_user_vote = Vote.new(user_id: "very bad", work_id: works(:gump).id)
+      expect {
+        bad_user_vote.voter_name
+      }.must_raise NoMethodError
+    end
+  end
+
+  describe "created_date" do
+    it "must return a formatted string of the date" do
+      vote.save 
+      
+      expect(vote.created_date).must_equal "#{vote.created_at.strftime("%B %d, %Y")}"
+    end
   end
 end
