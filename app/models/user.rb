be29b7ca404ab47f6a56071e312d8da6ea5267d9
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-04-29 16:52:07 -0700

    voted_date will return nil if the user didn't vote for the given work

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,11 @@ class User < ApplicationRecord
 
   def voted_date(work)
     vote_array = Vote.where({user_id: self.id, work_id: work.id})
-    date = vote_array[0].created_at.strftime("%B %d, %Y")
+    if vote_array.length > 0
+      date = vote_array[0].created_at.strftime("%B %d, %Y")
+    else
+      return nil
+    end
     return date
   end
 end
