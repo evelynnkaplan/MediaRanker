2d25b68257c653550009d0237f307ae19dd2649c
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-04-29 15:46:41 -0700

    wrote tests for add_vote

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,7 +1,52 @@
 require "test_helper"
 
 describe VotesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  before do
+    @gump = Work.create!(
+      category: "movie",
+      title: "Forrest Gump",
+      creator: "Tom Hanks",
+      pub_year: 1981,
+      description: "Why is this movie soooo sad?",
+    )
+  end
+
+  describe "add_vote" do
+    it "adds a vote if an existing user hasn't voted for the work yet" do
+      user = perform_login
+
+      post vote_work_path(@gump.id)
+
+      expect(@gump.votes.count).must_equal 1
+      expect(user.votes.count).must_equal 1
+
+      must_respond_with :redirect
+      must_redirect_to work_path(@gump)
+    end
+
+    it "won't add the vote if the existing user has already voted for that work" do
+      user = perform_login
+
+      post vote_work_path(@gump.id)
+
+      expect(@gump.votes.count).must_equal 1
+      expect(user.votes.count).must_equal 1
+
+      expect { post vote_work_path(@gump.id) }.wont_change "#{@gump.votes.count}"
+
+      expect(@gump.votes.count).must_equal 1
+      expect(user.votes.count).must_equal 1
+
+      must_respond_with :redirect
+      must_redirect_to work_path(@gump.id)
+    end
+
+    it "won't let a user vote if they're not logged in" do
+      expect { post vote_work_path(@gump.id) }.wont_change "#{@gump.votes.count}"
+      expect(@gump.votes.count).must_equal 0
+
+      must_respond_with :redirect
+      must_redirect_to work_path(@gump.id)
+    end
+  end
 end
