14ea001da4639da6f7664541269ec6a890df6fa0
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-04-29 19:36:58 -0700

    wrote model tests

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
+require "pry"
 
 describe Work do
   let(:work) { Work.new }
+  let(:gump) { works(:gump) }
+  let(:user_one) { users(:one) }
+  let(:user_two) { users(:two) }
 
   it "must be valid" do
     value(work).must_be :valid?
@@ -14,6 +18,91 @@ describe Work do
       expect(categories).must_include("album")
       expect(categories).must_include("book")
       expect(categories).must_include("movie")
+    end
+  end
+
+  describe "highlight" do
+    it "returns the work with the most votes" do
+      Vote.create!(work: gump, user: user_one)
+      Vote.create!(work: gump, user: user_two)
+
+      highlight = Work.highlight
+
+      expect(highlight.title).must_equal "Forrest Gump"
+    end
+
+    it "returns the first work if vote counts are equal" do
+      first = Work.first
+      highlight = Work.highlight
+
+      expect(highlight).must_equal first
+    end
+  end
+
+  describe "top_works" do
+    it "returns a list of the top 10 works in a category" do
+      book_titles = ["hp01", "hp02", "hp03", "hp04", "hp05", "hp06", "hp07", "hp08", "hp09", "hp10"]
+
+      book_titles.each do |title|
+        harry = Work.create!(category: "book", title: title, creator: "JK Rowling", pub_year: 2001, description: "You're a wizard")
+        Vote.create!(work: harry, user: user_one)
+      end
+
+      Vote.create!(work: Work.find_by(title: "hp02"), user: user_two)
+
+      top_books = Work.top_works("book")
+      top_book_titles = top_books.map do |book|
+        book.title
+      end
+
+      expect(top_book_titles.first).must_equal "hp02"
+      expect(top_book_titles.sort).must_equal book_titles
+    end
+
+    it "returns a list of less than 10 works if there are less than 10 works" do
+      book_titles = ["hp01", "hp02", "hp03", "hp04"]
+
+      book_titles.each do |title|
+        harry = Work.create!(category: "book", title: title, creator: "JK Rowling", pub_year: 2001, description: "You're a wizard")
+        Vote.create!(work: harry, user: user_one)
+      end
+
+      top_books = Work.top_works("book")
+      expect(top_books.count).must_equal 4
+    end
+
+    it "returns an empty array if there are no works in that category" do
+      top_cars = Work.top_works("car")
+
+      expect(top_cars).must_equal []
+    end
+  end
+
+  describe "vote_count" do
+    it "returns a work's vote count" do
+      Vote.create!(work: gump, user: user_one)
+
+      expect(gump.vote_count).must_equal 1
+    end
+  end
+
+  describe "category_all" do
+    it "can return all the works in a category" do
+      expect(Work.category_all("movie").count).must_equal 2
+    end
+
+    it "returns an empty array if there are no works in that category" do
+      top_cars = Work.category_all("car")
+
+      expect(top_cars).must_equal []
+    end
+  end
+
+  describe "created_date" do
+    it "must return a formatted string of the date" do
+      work.save
+
+      expect(work.created_date).must_equal "#{work.created_at.strftime("%B %d, %Y")}"
     end
   end
 end
